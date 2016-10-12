b050aa8f5c2d725d869186231c899226cb456f49
mcflugen <mcflugen@gmail.com>
2016-10-11 23:04:11 -0600

    Change build script to not hide homebrew.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,4 @@
-if [ `uname` == 'Darwin' ]; then
-  (cd /usr/local && mv bin bin.hide && mv lib lib.hide && mv include include.hide)
-fi
-
 mkdir _build && cd _build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 make all -j4
 make install
-
-if [ `uname` == 'Darwin' ]; then
-  (cd /usr/local && mv bin.hide bin && mv lib.hide lib && mv include.hide include)
-fi
